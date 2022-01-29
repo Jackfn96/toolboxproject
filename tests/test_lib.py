609d60a63c5db0f05e551088286fb92525d2c7cd
Jack Fraser-Nash <jackfn96@gmail.com>
2022-01-29 14:52:20 +0000

    fix tests

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,7 +1,7 @@
 from toolboxproject.lib import greet, who_am_i
 
 def test_greet():
-    assert type(greet('Jack')) == int
+    assert type(greet('Jack')) == str
 
 def test_who_am_i():
     assert type(who_am_i('Jack')) == str
